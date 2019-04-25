f4b0a76caea8d7d29b2451e15327ed28b8ee2921
Lemon <martatorrecilla@hotmail.com>
2019-04-25 14:00:16 +0200

    Editar y eliminar funcionales

--- Nodejs/DataBase/app.js
+++ Nodejs/DataBase/app.js
@@ -53,9 +53,18 @@ app.get('/seleccion', function(req, res){
 // Si viene por post, los atributos están en body, no en query(get)
 app.post('/seleccion', function(req, res){
   tipo.findOne({Tipo:req.body.tipo}, function (err, datos){
-    if(datos==undefined)
-    var nuevoTipo = new tipo({Tipo:req.body.tipo});
-    nuevoTipo.save(function (err, hecho){});
+    // con dos iguales, busco entre los documentos que hay (uno por uno) en la base uno que contenga el tipo que busco
+    // con tres iguales, busca un documento que contenga ese tipo. Si ninguno lo contiene, no devuelve nada.
+    /*Ej: var numero = 4;
+          var numerob = "4";
+          Si hago 4==numerob, funcionará, porque no importa que el tipo de dato (numero o string) coincida.
+          Sin embargo 4===numerob no será posible porque el tipo no coincide, sin embargo 4===numero si es cierto
+      */
+    //if(datos==undefined){ --> menos seguro, datos es un objeto, si está vacío, tiene tipo(objeto) pero no contenido
+    if(datos===null){
+      var nuevoTipo = new tipo({Tipo:req.body.tipo});
+      nuevoTipo.save(function (err, hecho){});
+    }
   });
 
   console.log(req.body.nombre);
@@ -70,8 +79,35 @@ app.post('/seleccion', function(req, res){
   });
 });
 
+/* Peticiones por PUT */
+app.put('/seleccion', function(req, res){
 
+  tipo.findOne({Tipo:req.body.tipo}, function (err, datos){
+    if(datos===null){
+      var nuevoTipo = new tipo({Tipo:req.body.tipo});
+      nuevoTipo.save(function (err, hecho){});
+    }
+  });
 
+  //primero: el elemento a editar, aquel que tenga la id enviada por el body en método put
+  //segundo: lo que vamos a editar, el nuevo contenido
+  pokemon.updateOne({_id: req.body.id}, {Nombre:req.body.nombre,
+                                         Tipo: req.body.tipo,
+                                         Imagen: req.body.imagen,
+                                         Descripción: req.body.descripcion}, function(err, editado){
+                                           if (err) {return res.send('No se ha podido editar este Pokèmon.');}
+                                           if (editado) {return res.send('¡Pokèmon actualizado!');}
+                                         });
+});
+
+/* Peticiones por DELETE */
+app.delete('/seleccion', function(req, res){
+
+  pokemon.deleteOne({_id: req.query.id}, function(err, eliminado){
+    if(err) {return res.send('No se ha podido eliminar este Pokèmon.');}
+    if(eliminado) {return res.send('Pokèmon eliminado.');}
+  });
+});
 
 
 var server = app.listen(8080, function()
